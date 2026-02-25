542407d97a183c370061fa043aec75ef5dda33d2
nwrsadat <anwarsadat.juni@gmail.com>
2026-02-25 11:33:27 +0700

    Initialize Spring Boot project structure

---
+++ src/test/java/com/anwar/salesinventory/SalesinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anwar.salesinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
