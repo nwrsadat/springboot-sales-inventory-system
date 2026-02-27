c778314c37c94d83436d02ed6069ac753d1a81a4
nwrsadat <anwarsadat.juni@gmail.com>
2026-02-27 10:01:41 +0700

    Integrate MyBatis and verify database connectivity

--- src/main/java/com/anwar/salesinventory/SalesInventorySystemApplication.java
+++ src/main/java/com/anwar/salesinventory/SalesInventorySystemApplication.java
@@ -1,8 +1,10 @@
 package com.anwar.salesinventory;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.anwar.salesinventory.mapper")
 @SpringBootApplication
 public class SalesInventorySystemApplication {
 
